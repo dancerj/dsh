da9a01b517845dce1ff885b6a91e32f19bcd2b4f
Junichi Uekawa <dancer@netfort.gr.jp>
2008-08-03 08:06:39 +0900

    add some more annotations on what kind of debugging tools I can use.

--- tests/pipe-vs-redirect.sh
+++ tests/pipe-vs-redirect.sh
@@ -10,17 +10,19 @@ t2=$(tempfile)
 echo expecting: $nmachines
 echo For pipe:
 ./dsh -m ${machines} -M -F 20 -r./tests/sleepyshell hostname | sort > "$t1" ; wc -l "$t1"
+cat "$t1" | sort -n | cut -d: -f1 | awk 'BEGIN{i=1} {if($1 != i) {print i" missing"; i++} i++}'
 echo For redirect:
 ./dsh -m ${machines} -M -F 20 -r./tests/sleepyshell hostname > "$t2" ; wc -l "$t2"
+cat "$t2" | sort -n | cut -d: -f1 | awk 'BEGIN{i=1} {if($1 != i) {print i" missing"; i++} i++}'
 
 [ $(cat "$t1"| wc -l ) = "$nmachines" ]
 [ $(cat "$t2"| wc -l ) = "$nmachines" ]
 
 rm "$t1" "$t2"
 
-
-
 # Above code is shorter than ideal. The following is too slow, but
 # this is more reliable.
 
 # ./dsh -m $(echo $(seq 1 3000 ) | sed 's/ /,/g')  -M -F 20 -r./tests/sleepyshell hostname  > "/tmp/test" ; wc -l "/tmp/test"
+
+# cat  | sort -n | cut -d: -f1 | awk 'BEGIN{i=1} {if($1 != i) {print i" missing"; i++} i++}'
