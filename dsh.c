6263208bb7854bfba27527a9ae7186e39c648f34
Junichi Uekawa <dancer@netfort.gr.jp>
2008-08-02 11:12:59 +0900

    add year 2008

--- dsh.c
+++ dsh.c
@@ -1,6 +1,6 @@
 /*
  *  DSH / dancer's shell or the distributed shell
- *  Copyright (C) 2001-2007 Junichi Uekawa
+ *  Copyright (C) 2001-2008 Junichi Uekawa
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
